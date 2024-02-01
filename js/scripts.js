b4959f6e86d2780f5952bad55f892b6337c48973
Yuji <yujiasofu@gmail.com>
2024-02-01 13:37:43 -0300

    Adjusting text

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector("#form")
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
-    // Todos os campos preenchidos, envia o form.
-    form.submit();
-});
