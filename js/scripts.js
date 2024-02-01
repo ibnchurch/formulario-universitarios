619f7c4a124d949faa35ab5e684e8b2349c1c995
Yuji <yujiasofu@gmail.com>
2024-02-01 01:16:15 -0300

    Fixing phone and inserting top bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,8 @@
 const form = document.querySelector("#form")
-const nameInput = document.querySelector('#fullName')
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    // Verificar nome vazio
-    if (nameInput.value === "") {
-    alert("Por favor, preencha seu nome");
-    return;
-    }
-
+    
     // Todos os campos preenchidos, envia o form.
     form.submit();
 });
-
-// Função validar email
-function isEmailValid(email) {
-    //Cria regex para validar email
-    const emailRegex = new RegExp(
-        //
-    )
-}
